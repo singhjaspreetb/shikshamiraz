f96a19401655d8c31620a86ad63299a7cb3a8cdc
raipratik <pratikkumar399@gmail.com>
2023-03-20 18:42:11 +0530

    modification

--- lib/screens/home/widgets/active_course.dart
+++ lib/screens/home/widgets/active_course.dart
@@ -3,62 +3,63 @@ import 'package:shikshamiraz/screens/home/widgets/category_title.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 class ActiveCourse extends StatelessWidget {
+  const ActiveCourse({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // width: 55,
-        child: Column(
+    return Column(
       children: [
         CategoryTitle("Active Courses", "View All courses"),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Container(
-          margin: EdgeInsets.all(25),
+          margin: const EdgeInsets.all(25),
           decoration: BoxDecoration(
               color: Colors.grey.withOpacity(0.1),
               border: Border.all(color: Colors.grey.withOpacity(0.1), width: 1),
               borderRadius: BorderRadius.circular(8)),
           child: Row(children: [
-            Row(
-              children: [
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child:
-                        Image.asset('assets/images/physics.png', width: 100)),
-                SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Laws of Motion',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    Text("2 more lessons left",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ))
-                  ],
+            // Row(
+            //   children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset('assets/images/physics.png', width: 100)),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Laws of Motioniiiiii',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
+                Text("2 more lessons left",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ))
               ],
             ),
-            CircularPercentIndicator(
-              radius: 40,
-              lineWidth: 5.0,
-              percent: 0.75,
-              center: Text(
-                "75%",
-                style: TextStyle(fontWeight: FontWeight.bold),
+            //   ],
+            // ),
+            Expanded(
+              child: CircularPercentIndicator(
+                radius: 40,
+                lineWidth: 5.0,
+                percent: 0.75,
+                center: const Text(
+                  "75%",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                progressColor: Colors.amber,
               ),
-              progressColor: Colors.amber,
             ),
           ]),
-        )
+        ),
       ],
-    ));
+    );
   }
 }
--- lib/screens/home/widgets/course_item.dart
+++ lib/screens/home/widgets/course_item.dart
@@ -3,91 +3,89 @@ import 'package:shikshamiraz/model/courses.dart';
 
 class CourseItem extends StatelessWidget {
   final Course courses;
-  CourseItem(this.courses);
+  const CourseItem(this.courses, {super.key});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            height: 380,
-            width: 197,
-            decoration: BoxDecoration(
-                color: Color.fromARGB(255, 250, 233, 233),
-                borderRadius: BorderRadius.circular(15)),
-            child: Column(
-              children: [
-                Expanded(
-                    flex: 2,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: const Radius.circular(20),
-                      ),
-                      child: Image.asset(
-                        courses.imgPath,
-                        fit: BoxFit.cover,
-                      ),
-                    )),
-                SizedBox(height: 8),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          courses.course,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
+    return Stack(
+      children: [
+        Container(
+          height: 380,
+          width: 197,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(0, 250, 233, 233),
+              borderRadius: BorderRadius.circular(15)),
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
                     ),
-                    Row(
-                      children: [
-                        Text(courses.length,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.black)),
-                        Container(
-                          margin: EdgeInsets.symmetric(horizontal: 5),
-                          width: 5,
-                          height: 5,
-                          decoration: BoxDecoration(
-                              color: Colors.amber, shape: BoxShape.circle),
+                    child: Image.asset(
+                      courses.imgPath,
+                      fit: BoxFit.cover,
+                    ),
+                  )),
+              const SizedBox(height: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        courses.course,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
-                        Text(
-                          '2 h 32 minutes',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            color: Colors.black,
-                          ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(courses.length,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.black)),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        width: 5,
+                        height: 5,
+                        decoration: const BoxDecoration(
+                            color: Colors.amber, shape: BoxShape.circle),
+                      ),
+                      const Text(
+                        '2 h 32 minutes',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.black,
                         ),
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
           ),
-          Positioned(
-              bottom: 40,
-              right: 30,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.blue,
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    )),
-                onPressed: () {},
-                child: Text('Start'),
-              ))
-        ],
-      ),
+        ),
+        Positioned(
+            bottom: 40,
+            right: 30,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  )),
+              onPressed: () {},
+              child: const Text('Start'),
+            ))
+      ],
     );
   }
 }
