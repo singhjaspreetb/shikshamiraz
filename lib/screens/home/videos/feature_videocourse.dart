8c3389bf978d2f0f7ef3720ff3633786aec670dd
raipratik <pratikkumar399@gmail.com>
2023-03-21 04:31:23 +0530

    made seprate folder for videos

--- lib/screens/home/videos/feature_videocourse.dart
+++ lib/screens/home/videos/feature_videocourse.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shikshamiraz/model/courses.dart';
 import 'package:shikshamiraz/screens/home/widgets/category_title.dart';
 import 'package:shikshamiraz/screens/home/widgets/course_item.dart';
-import 'package:shikshamiraz/screens/home/widgets/course_videoitem.dart';
+import 'package:shikshamiraz/screens/home/videos/course_videoitem.dart';
 
 class FeatureVideoCourse extends StatelessWidget {
   final courseList = Course.generateCourse();
