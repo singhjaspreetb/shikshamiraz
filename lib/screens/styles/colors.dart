95aa37c29acd43cf19c23aa6acbea5e256fb1101
raipratik <pratikkumar399@gmail.com>
2023-03-21 04:49:49 +0530

    added color

--- lib/screens/styles/colors.dart
+++ lib/screens/styles/colors.dart
@@ -5,5 +5,5 @@ abstract class AppColors {
   static const Color orange = Color(0xFFFFAA5B);
   static const Color purple = Color(0xFF9AA0FC);
   static const Color pink = Color(0xFFFBAEAE);
-  static const Color green = Color(0xFF71CFB9);
+  static const Color green = Color(0xFFE1F396);
 }
