da8522b64f65f928add6949ef0d11672d25b2144
raipratik <pratikkumar399@gmail.com>
2023-03-21 08:30:24 +0530

    added yt net permission

--- lib/screens/home/videos/youtube_player.dart
+++ lib/screens/home/videos/youtube_player.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class YoutubePlayerExample extends StatefulWidget {
@@ -12,44 +13,43 @@ class YoutubePlayerExample extends StatefulWidget {
 class _YoutubePlayerExampleState extends State<YoutubePlayerExample> {
   final video = "https://www.youtube.com/watch?v=glDU96h8A48";
 
-  late YoutubePlayerController _controller;
+  YoutubePlayerController? _controller;
 
   @override
   void initState() {
+    super.initState();
     final videoId = YoutubePlayer.convertUrlToId(video);
 
     _controller = YoutubePlayerController(
-        initialVideoId: videoId!,
-        flags: const YoutubePlayerFlags(
-          autoPlay: false,
-        ));
-    super.initState();
+      initialVideoId: "https://www.youtube.com/watch?v=glDU96h8A48",
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: true,
+        isLive: false,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("Laws of Motion")),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            YoutubePlayer(
-              controller: _controller,
-              showVideoProgressIndicator: true,
-              onReady: () => debugPrint("Ready"),
-              bottomActions: [
-                CurrentPosition(),
-                ProgressBar(
-                  isExpanded: true,
-                  colors: const ProgressBarColors(
-                    playedColor: Colors.amber,
-                    handleColor: Colors.amberAccent,
-                  ),
-                ),
-                const PlaybackSpeedButton(),
-              ],
-            )
-          ],
-        ));
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller!,
+        showVideoProgressIndicator: true,
+        progressIndicatorColor: Colors.amber,
+        progressColors: ProgressBarColors(
+          playedColor: Colors.amber,
+          handleColor: Colors.amberAccent,
+        ),
+      ),
+      builder: (context, player) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Youtube Player"),
+          ),
+          body: player,
+        );
+      },
+    );
   }
 }
